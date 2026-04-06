52dc6e01772223c72527d1fad562510aa2976e24
Saikat-too <saikatsingha00q@gmail.com>
2026-04-06 11:19:38 +0600

    feat: connect with prisma mysql & add database for players route

--- test_project/src/players/players.controller.ts
+++ test_project/src/players/players.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Req } from '@nestjs/common';
 import { PlayersService } from './players.service';
 import { Body, Post } from '@nestjs/common';
 import type { CreatePlayerDto, PlayerResponseDto } from '../dto/players.dto';
@@ -8,17 +8,26 @@ export class PlayersController {
   constructor(private readonly playerService: PlayersService) {}
 
   @Get()
-  getPlayers(): any {
+  async getPlayers(): Promise<PlayerResponseDto[]> {
     return this.playerService.getPlayers();
   }
 
   @Get(':id')
-  getPlayerById(@Param('id') id: string): PlayerResponseDto {
-    return this.playerService.getPlayerById(id);
+  async getPlayerById(
+    @Param('id') id: string,
+  ): Promise<PlayerResponseDto | null> {
+    const playerId = parseInt(id);
+    return this.playerService.getPlayer({ id: playerId });
   }
 
   @Post()
-  createPlayer(@Body() playerData: CreatePlayerDto): PlayerResponseDto {
+  async createPlayer(
+    @Body() playerData: CreatePlayerDto,
+  ): Promise<PlayerResponseDto> {
     return this.playerService.createPlayer(playerData);
+  }
+  @Get()
+  getRequestInfo(@Req() request: Request): string {
+    return `Request method: ${request.method}, Request URL: ${request.url}`;
   }
 }
--- test_project/src/players/players.service.ts
+++ test_project/src/players/players.service.ts
@@ -1,29 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePlayerDto, PlayerResponseDto } from '../dto/players.dto';
+import { PrismaService } from '../prisma.service';
+import { Players } from '../../generated/prisma/client';
 
 @Injectable()
 export class PlayersService {
-  private readonly players = [
-    { id: 1, name: 'Saikat', age: 24 },
-    { id: 2, name: 'Ashini', age: 32 },
-  ];
+  constructor(private prisma: PrismaService) {}
 
-  getPlayers(): any {
-    return this.players;
-  }
-  getPlayerById(id: string): PlayerResponseDto {
-    const player = this.players.find((player) => player.id === parseInt(id));
-    if (!player) {
-      throw new Error('Player not found');
-    }
+  async createPlayer(data: CreatePlayerDto): Promise<PlayerResponseDto> {
+    const player = await this.prisma.players.create({ data });
     return new PlayerResponseDto(player.id, player.name, player.age);
   }
-  createPlayer(playerData: CreatePlayerDto): PlayerResponseDto {
-    const newPlayer = {
-      id: this.players.length + 1,
-      ...playerData,
-    };
-    this.players.push(newPlayer);
-    return new PlayerResponseDto(newPlayer.id, newPlayer.name, newPlayer.age);
+
+  async getPlayers(): Promise<PlayerResponseDto[]> {
+    const players = await this.prisma.players.findMany();
+    return players.map(
+      (player) => new PlayerResponseDto(player.id, player.name, player.age),
+    );
+  }
+
+  async getPlayer(userWhereUniqueInput: {
+    id: number;
+  }): Promise<Players | null> {
+    const player = await this.prisma.players.findUnique({
+      where: userWhereUniqueInput,
+    });
+    if (!player) {
+      throw new Error(`Player with ID ${userWhereUniqueInput.id} not found`);
+    }
+    return new PlayerResponseDto(player.id, player.name, player.age);
   }
 }
